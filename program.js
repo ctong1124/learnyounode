8867ccd648b12f2505626d4973c634ae40b0f554
Carisa Tong <ctong1124@gmail.com>
2017-05-14 21:20:22 -0400

    lesson 2

--- program.js
+++ program.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+//console.log(process.argv);
+
+var args = process.argv;
+var sum=0;
+
+for (var i=2; i < process.argv.length; i++) {
+	//console.log(args[i]);
+	//console.log(typeof(Number(args[i])));
+	sum = sum + Number(args[i]);
+	//console.log(sum);
+}
+
+console.log(sum);
