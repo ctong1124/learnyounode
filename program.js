b35d1382594c1db5d7dacac4c1ba77025d116be3
Carisa Tong <ctong1124@gmail.com>
2017-05-23 16:51:44 -0400

    lesson 8

--- program.js
+++ program.js
@@ -1,11 +1,24 @@
 var fs = require('fs');
 var http = require('http');
+var bl = require('bl');
 
-var mymodule = require('./mymodule.js')  
+http.get(process.argv[2], function(response) {
+	
+	response.pipe(bl(function(err, data) {
+		if(err){
+			console.log("Error");
+		}
+		var string = data.toString();
+		
+		console.log(string.length);
+		console.log(string);
+	}
+		));
+	
 
-mymodule(process.argv[2], function(response){
+}) 
 
-});
+
 
 
 
