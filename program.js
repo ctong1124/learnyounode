613d8152904141e09675c023159a4b412569056f
Carisa Tong <ctong1124@gmail.com>
2017-05-23 17:21:47 -0400

    lesson 9

--- program.js
+++ program.js
@@ -2,24 +2,36 @@ var fs = require('fs');
 var http = require('http');
 var bl = require('bl');
 
-http.get(process.argv[2], function(response) {
-	
-	response.pipe(bl(function(err, data) {
-		if(err){
-			console.log("Error");
+var arr = [];
+var count = 0;
+
+
+for (var i=0; i<3; i++) {
+	get(i);
+}
+
+function get(index) {
+	http.get(process.argv[2+index], function(response) {
+		response.pipe(bl(function(err, data) {
+			if(err){
+				console.log("Error");
+			}
+			arr[index] = data.toString()
+			
+
+			count++;
+
+			if(count==3) {
+				print();
+			}
+
 		}
-		var string = data.toString();
-		
-		console.log(string.length);
-		console.log(string);
+		));	
+	})
+}
+
+function print() {
+	for (var i=0; i<arr.length; i++) {
+		console.log(arr[i]);
 	}
-		));
-	
-
-}) 
-
-
-
-
-
-
+}
