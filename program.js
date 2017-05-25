2ecb3463a9b336d597a0ce06ec660fe531691d57
Carisa Tong <ctong1124@gmail.com>
2017-05-25 13:29:47 -0400

    lesson 11

--- program.js
+++ program.js
@@ -1,27 +1,14 @@
-var net = require('net') 
+var http = require('http')  
+var fs = require('fs');
 
+var server = http.createServer(function (request, response) { 
+	response.writeHead(200, { 'content-type': 'text/plain' })
+    var file = fs.createReadStream(process.argv[3]);
+    file.pipe(response);
 
-var server = net.createServer(function (socket) {  
-	var date = new Date();
+});  
+server.listen(process.argv[2])  
 
-	socket.write(date.getFullYear()+ "-" + 
-		zeroFill(date.getMonth() + 1)+ "-" + 
-		zeroFill(date.getDate()) + " " + 
-		zeroFill(date.getHours()) + ":" + 
-		zeroFill(date.getMinutes()) + "\n");
-
-	socket.end();
-})  
-server.listen(process.argv[2])
-
-
-function zeroFill(num) {
-	var str = num.toString();
-	if (str.length <2) {
-		str = "0" + str;
-}
-	return str;
-}
 
 
 
