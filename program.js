10bcc6a5333fef276ba66892e77166fe468908eb
Carisa Tong <ctong1124@gmail.com>
2017-05-25 15:37:16 -0400

    last lesson

--- program.js
+++ program.js
@@ -1,20 +1,32 @@
 var http = require('http');
 var map = require('through2-map')
+var url = require('url');
 
 //process.argv[3] is the response
 var server = http.createServer(function (request, response) { 
+	var headers = request.headers;
 	var method = request.method;
+	var parse = url.parse(request.url, true)
+	var pathname = parse.pathname;
 
-	if (method == "POST") {
-		response.writeHead(200, { 'content-type': 'text/plain' })
-		request.pipe(map(function (chunk) {  
-      	 return chunk.toString().toUpperCase()  
-    	 })).pipe(response); 
+	if (method == "GET") {
+		var date = new Date(parse.query.iso);
+		var json = {};
+		if (pathname.includes("parsetime")) {
+			json["hour"] = date.getHours();
+			json["minute"] = date.getMinutes();
+			json["second"] = date.getSeconds();
+		}
+		if (pathname.includes("unixtime")) {
+			json["unixtime"] = date.getTime();
+		}
+		response.writeHead(200, { 'Content-Type': 'application/json' });
+	    response.write(JSON.stringify(json));
+	    response.end();
 	}
 	else {
-		 return response.end('send me a POST\n')
+		 return response.end('send me a GET\n')
 	}
-
 });  
 server.listen(process.argv[2])  
 
