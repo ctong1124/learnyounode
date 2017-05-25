004d07549c5e7bf94baa42d28fbc2ed3048132fc
Carisa Tong <ctong1124@gmail.com>
2017-05-25 13:57:53 -0400

    lesson 10 with extra info

--- program.js
+++ program.js
@@ -1,10 +1,41 @@
-var http = require('http')  
-var fs = require('fs');
+var http = require('http');
+var map = require('through2-map')
 
+//process.argv[3] is the response
 var server = http.createServer(function (request, response) { 
-	response.writeHead(200, { 'content-type': 'text/plain' })
-    var file = fs.createReadStream(process.argv[3]);
-    file.pipe(response);
+	//var headers = request.headers;
+	var method = request.method;
+	// var url = request.url;
+	// var body = []; 
+
+
+
+	if (method == "POST") {
+		response.writeHead(200, { 'content-type': 'text/plain' })
+		request.pipe(map(function (chunk) {  
+      	 return chunk.toString().toUpperCase()  
+    	 })).pipe(response); 
+	}
+	else {
+		 return response.end('send me a POST\n')
+	}
+
+	 
+
+
+	//response
+	// response.statusCode = 200;
+ //    response.setHeader('Content-Type', 'application/json');
+ //    var responseBody = {
+ //      headers: headers,
+ //      method: method,
+ //      url: url,
+ //      body: body
+ //    };
+
+
+    // response.write(JSON.stringify(responseBody));
+    // response.end();
 
 });  
 server.listen(process.argv[2])  
